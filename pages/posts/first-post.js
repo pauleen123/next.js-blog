df4acfe2f79baf205f00862ac48999c98ffab22f
Pauline Muthoni <paugitonga6@gmail.com>
2024-03-10 15:18:03 +0300

    change a few ines on the about me section and added some class names for design later

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -11,15 +11,11 @@ export default function FirstPost(){
     <title>First Post</title>
    </Head>
     
-    <h1>Get to know me!</h1>
-     <p>My name is Pauline Muthoni Gitonga. </p>
-     <p>I have always loved technology and computers and 
-      after dabbling in the world of Information Technology i 
-      have settled in the field of Full-Stack Software Development. 
-      I love to build and develop good looking and functioning web 
-      applications </p>
-     <p>Check out this link to my personal portfolio to get to see my 
-      projects and skills.</p>
+    <h1 className="h1">Get to know me!</h1>
+     <p className="p1">My name is Pauline Muthoni Gitonga. </p>
+     <p className="p2">Welcome to my <span color="red">life</span> </p>
+     <p className="p3">Check out this link to my personal portfolio to get to see my 
+      projects and skills in Software Engineering! 😀</p>
   </Layout>
 );
 }
